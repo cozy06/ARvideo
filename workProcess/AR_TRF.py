cf2ca01f24504d7ab860de63b6427e0f0f4785c7
cozy06 <jamesjin06@gmail.com>
2023-08-08 00:42:05 +0900

    functioning curve Img

--- workProcess/AR_TRF.py
+++ workProcess/AR_TRF.py
@@ -11,25 +11,25 @@ videoSource = "/Users/cozy06/Desktop/cozy06-Dev/python/ARvideo/data/testVideo.mp
 WINDOW_NAME = "AR Viewer"
 Frame_Size = 400
 
-mapper = fisheyeImgConv()
+FOV = 90
+theta = 0
+phi = 0
 
-cap = cv2.VideoCapture(videoSource)
+def mapping(exp, Frame_size):
+    mapy, mapx = np.indices((Frame_Size, Frame_Size), dtype=np.float32)
 
-exp = 1.5  # 볼록, 오목 지수 (오목 : 0.1 ~ 1, 볼록 : 1.1~)
+    mapx = 2 * mapx / (Frame_Size - 1) - 1
+    mapy = 2 * mapy / (Frame_Size - 1) - 1
 
-mapy, mapx = np.indices((Frame_Size, Frame_Size), dtype=np.float32)
+    r, theta = cv2.cartToPolar(mapx, mapy)
+    r = r ** exp
 
-mapx = 2 * mapx / (Frame_Size - 1) - 1
-mapy = 2 * mapy / (Frame_Size - 1) - 1
+    mapx, mapy = cv2.polarToCart(r, theta)
 
-r, theta = cv2.cartToPolar(mapx, mapy)
-r = r ** exp
+    mapx = ((mapx + 1) * Frame_Size - 1) / 2
+    mapy = ((mapy + 1) * Frame_Size - 1) / 2
 
-mapx, mapy = cv2.polarToCart(r, theta)
-
-mapx = ((mapx + 1) * Frame_Size - 1) / 2
-mapy = ((mapy + 1) * Frame_Size - 1) / 2
-
+    return mapx, mapy
 
 def movement():
     global FOV, theta, phi, Thread
@@ -46,18 +46,21 @@ def movement():
 inputThread = threading.Thread(target=movement)
 inputThread.start()
 
-FOV = 90
-theta = 0
-phi = 0
+
+mapper = fisheyeImgConv()
+
+cap = cv2.VideoCapture(videoSource)
 
 while Thread:
     _, img = cap.read()
 
-    cv2.imshow("input image", img)
-
     outputFlat = mapper.eqruirect2persp(img, FOV, theta, phi, Frame_Size, Frame_Size)
+
+    exp = 1.5  # 볼록, 오목 지수 (오목 : 0.1 ~ 1, 볼록 : 1.1~)
+    mapx, mapy = mapping(exp, Frame_Size)
     outputCurve = cv2.remap(outputFlat, mapx, mapy, cv2.INTER_LINEAR)
 
+    cv2.imshow("input image", img)
     cv2.imshow(WINDOW_NAME + " Flatten", outputFlat)
     cv2.imshow(WINDOW_NAME + " Curved", outputCurve)
     cv2.waitKey(1)
